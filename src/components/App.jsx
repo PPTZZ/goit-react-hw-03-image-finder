12fbe62e253bc4e0c9290dfd1473d3ceb5f69c40
unknown <poputa.alex@gmail.com>
2024-10-23 13:46:20 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import ImageGallery from './ImageGallery';
 import Loader from './Loader';
 import SearchBar from './SearchBar';
 import { Component } from 'react';
+import Modal from './Modal';
 
 const END_POINT = 'https://pixabay.com/api/';
 // eslint-disable-next-line react-refresh/only-export-components
@@ -10,13 +11,19 @@ const KEY = '45639968-4ab0c3e34d3afa9a12b28af2f';
 
 export default class App extends Component {
 	async componentDidUpdate(prevProps, prevState) {
-		if (prevState.filter !== this.state.filter) {
+		if (
+			prevState.filter !== this.state.filter ||
+			prevState.page !== this.state.page
+		) {
 			try {
+				console.log('fetching data');
 				this.handleLoader();
 				const response = await axios.get(
 					`${END_POINT}?key=${KEY}&q=${this.state.filter}&safesearch=true&page=${this.state.page}&per_page=${this.state.per_page}`
 				);
-				this.setState({ images: response.data.hits });
+				this.setState({
+					images: [...this.state.images, ...response.data.hits],
+				});
 			} catch (err) {
 				console.error(err);
 			} finally {
@@ -30,21 +37,36 @@ export default class App extends Component {
 		filter: '',
 		hasError: false,
 		modalIsOpen: false,
+		modalImg: '',
 		isLoading: false,
 		page: 1,
 		per_page: 12,
 	};
 
-	handleLoader() {
+	handleLoader = () => {
 		this.setState((prevState) => ({ isLoading: !prevState.isLoading }));
-	}
+	};
+
+	handleModal = () => {
+		this.setState((prevState) => ({ modalIsOpen: !prevState.modalIsOpen }));
+	};
 
 	handleSearch = (data) => {
+		this.setState({ images: [] });
+		this.setState({ page: 1 });
 		this.setState({ filter: data });
 	};
 
 	handleImages = (data) => {
 		this.setState({ images: data });
+	};
+
+	loadMore = () => {
+		this.setState((prevState) => ({ page: prevState.page + 1 }));
+	};
+
+	setModalImg = (data) => {
+		this.setState({ modalImg: data });
 	};
 
 	render() {
@@ -59,10 +81,20 @@ export default class App extends Component {
 				/>
 				<Loader loading={this.state.isLoading} />
 				{this.state.images.length > 0 ? (
-					<ImageGallery images={this.state.images} />
+					<ImageGallery
+						images={this.state.images}
+						loadMore={this.loadMore}
+						handleModal={this.handleModal}
+						setModalImg={this.setModalImg}
+					/>
 				) : (
-					<h1>Use the search bar to find images </h1>
+					<h1 style={{textAlign:'center', marginTop:'10px'}}>Use the search bar to find images </h1>
 				)}
+				<Modal
+					selectedImg={this.state.modalImg}
+					modalIsOpen={this.state.modalIsOpen}
+					handleModal={this.handleModal}
+				/>
 			</>
 		);
 	}
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,47 +1,67 @@
-import { Box, ImageList, ImageListItem, Stack } from '@mui/material';
+import { Box, Button, ImageList, ImageListItem, Stack } from '@mui/material';
 import PropTypes from 'prop-types';
 
-const ImageGallery = ({ images }) => {
-
-  const handleClick=(e)=>{
-    console.log(e.target.data);
-    
-  }
-
+const ImageGallery = ({ images, loadMore,handleModal,setModalImg }) => {
+	const handleClick = (e) => {
+    const selectedImg = images.find((image)=>image.webformatURL === e.target.src)
+		setModalImg(selectedImg)
+    handleModal()
+	};
 
 	return (
-		<Stack spacing={4}>
-			<Box sx={{ width: '98vw', height: 'calc(100% - 90px)', }}>
-				<ImageList sx={{
-          p:'5px',
-          width:'100%',
-          gridTemplateColumns:'repeat(auto-fill,minmax(280px, 1fr))!important',
-        }} >
+		<Stack spacing={4} direction='column'>
+			<Box sx={{ width: '98vw', height: 'calc(100% - 90px)' }}>
+				<ImageList
+					sx={{
+						p: '5px',
+						width: '100%',
+						gridTemplateColumns:
+							'repeat(auto-fill,minmax(280px, 1fr))!important',
+						overflowX: 'hidden',
+					}}
+				>
 					{images.map((image) => (
-						<ImageListItem onClick={handleClick}
-            sx={{
-              transition:'.2s',
-              '&:hover':{
-                cursor:'pointer',
-                scale:'1.05',
-                zIndex:'1',
-              }
-            }}
+						<ImageListItem
+							onClick={handleClick}
+							sx={{
+								transition: '.2s',
+								'&:hover': {
+									cursor: 'pointer',
+									scale: '1.05',
+									zIndex: '1',
+								},
+							}}
 							key={image.id}
-              title={image.tags}
-              data-large={image.largeImageURl}
-              
+							title={image.tags}
+							data-large={image.largeImageURl}
 						>
-							<img src={image.webformatURL} alt={image.tags} data-large={image.largeImageURl}/>
+							<img
+								src={image.webformatURL}
+								alt={image.tags}
+								data-large={image.largeImageURl}
+							/>
 						</ImageListItem>
 					))}
 				</ImageList>
 			</Box>
+			<Button
+				variant='contained'
+				sx={{
+					width:200,
+          alignSelf:'center'
+				}}
+				onClick={loadMore}
+			>
+				Load more images
+			</Button>
 		</Stack>
 	);
 };
 
 ImageGallery.propTypes = {
 	images: PropTypes.array,
+	loadMore: PropTypes.func,
+	handleModal: PropTypes.func,
+	setModalImg: PropTypes.func,
 };
 export default ImageGallery;
